351be5e35d14272a6227156c62d1e40f959ec93c
Abhisek-Das <babuju.abhisek@gmail.com>
2021-12-17 18:56:06 -0500

    Test for git

--- src/main/java/com/springboot/mvc/login/ProjectManagementAppApplication.java
+++ src/main/java/com/springboot/mvc/login/ProjectManagementAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectManagementAppApplication {
 
 	
-	//Test
+	//Test for git
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementAppApplication.class, args);
 	}
